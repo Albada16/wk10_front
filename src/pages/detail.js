053ea02821e091262d2ac9bfbbe8f2f92a150280
Albada16 <NamelessOAO@gmail.com>
2022-12-04 18:45:13 +0800

    update

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom"
 import React, { useEffect, useState } from 'react';
 import { getAriticleById } from "../api"
 
+
 const Detail = ()=> {
 	let {id} = useParams();
 
@@ -15,8 +16,10 @@ const Detail = ()=> {
     const data = await getAriticleById(id)
     setDetail(data)
   }
+  
 
 	return(
+		
 		<div>
 			{	detail ?
 				<div className="detail_wrap">
@@ -24,6 +27,16 @@ const Detail = ()=> {
 				<div className="detail_author">By {detail.author}</div>
 				<div className="detail_description_long">{detail.description_long}</div>
 				<img className="detail_img" alt="" src={detail.image}></img>
+				<div className="button">
+					<div className="like" onClick>
+						<img className="like-icon" alt="" src></img>
+						<div className="cont.like">1</div>
+					</div>
+					<div className="comm" onClick>
+						<img className="comm-icon" alt="" src></img>
+						<div className="cont.comm">1</div>
+					</div>
+				</div>
 			</div>
 				: <></>
 			}
