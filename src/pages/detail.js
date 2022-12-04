c4bedd67ab16cab9021e427156ccff6deba3a488
Albada16 <NamelessOAO@gmail.com>
2022-12-04 20:23:15 +0800

    update

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -1,43 +1,61 @@
 import { useParams } from "react-router-dom"
 import React, { useEffect, useState } from 'react';
 import { getAriticleById } from "../api"
-
+import heart from '../img/heart-3510.png'
+import comm from '../img/comment.png'
 
 const Detail = ()=> {
 	let {id} = useParams();
 
-  const [detail,setDetail] = useState()
-	
-  useEffect(()=>{
-    setDetailData()
-  },[id])
+  	const [detail,setDetail] = useState()
+	const [isActiveLike,setActiveLike] = useState(false)
+	const [isActiveComm,setActiveComm] = useState(false)
 
-  async function setDetailData() {
-    const data = await getAriticleById(id)
-    setDetail(data)
-  }
-  
+	useEffect(()=>{
+		setDetailData()
+	},[id])
+
+	async function setDetailData() {
+		const data = await getAriticleById(id)
+		setDetail(data)
+	}
+
+	const likeClick = event => {
+		setActiveLike(current => !current)
+		// console.log("123123")
+	}
+	const commClick = event => {
+		setActiveComm(current => !current)
+		// console.log("123123")
+	}
+	
 
 	return(
 		
 		<div>
 			{	detail ?
 				<div className="detail_wrap">
-				<div className="detail_title">{detail.title}</div>
-				<div className="detail_author">By {detail.author}</div>
-				<div className="detail_description_long">{detail.description_long}</div>
-				<img className="detail_img" alt="" src={detail.image}></img>
-				<div className="button">
-					<div className="like" onClick>
-						<img className="like-icon" alt="" src></img>
-						<div className="cont.like">1</div>
+					<div className="left_wrap">
+						<div className="detail_title">{detail.title}</div>
+						<div className="detail_author">By {detail.author}</div>
+						<div className="detail_description_long">{detail.description_long}</div>
+						<img className="detail_img" alt="" src={detail.image}></img>
+						<div className="button">
+							<button className="btn like" onClick={likeClick} >
+								<img className="icon like-icon" src={heart}></img>
+								<div className="cont-like">1</div>
+							</button>
+							<button className="btn comm" onClick={commClick}>
+								<img className="icon comm-icon" alt="" src={comm}></img>
+								<div className="cont-comm">1</div>
+							</button>
+						</div>
 					</div>
-					<div className="comm" onClick>
-						<img className="comm-icon" alt="" src></img>
-						<div className="cont.comm">1</div>
+					<div className="right_wrap">
+						<div className={ isActiveLike ? 'active_like' : 'like_none'}>11231231231212312312331223123123123</div>
+						<div className={ isActiveComm ? 'active_comm' : 'comm_none'}>11231231231212312312331223123123123</div>
 					</div>
 				</div>
-			</div>
 				: <></>
 			}
 		</div>
